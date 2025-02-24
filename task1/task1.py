c47d229564659cd5d7a196e8fd5f4bf9ddb45b5b
“YukiKuma111” <1343879876@qq.com>
2025-02-24 22:16:33 +0800

    black format task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -3,22 +3,23 @@ import open3d as o3d
 import datetime
 from scipy.spatial import KDTree
 
+
 def icp_core(point_cloud1, point_cloud2):
     """
     Solve transformation from point_cloud2 to point_cloud1, T1_2
     :param point_cloud1: numpy array, size = n x 3, n is num of point
     :param point_cloud2: numpy array, size = n x 3, n is num of point
     :return: transformation matrix T, size = 4x4
-    
+
     Note: point cloud should be in same size. Point with same index are corresponding points.
           For example, point_cloud1[i] and point_cloud2[i] are a pair of cooresponding points.
-    
+
     """
-    assert point_cloud1.shape == point_cloud2.shape, 'point cloud size not match'
-    
+    assert point_cloud1.shape == point_cloud2.shape, "point cloud size not match"
+
     T1_2 = np.eye(4)
     # TODO: Finish icp based on SVD, you can refer the lecture slides. Please leave comments and explainations for each step.
-    
+
     save_intermediate_result = False
 
     # 1. Calculate the centroid of both point clouds
@@ -60,6 +61,7 @@ def icp_core(point_cloud1, point_cloud2):
 
     return T1_2
 
+
 def transform_point_cloud(point_cloud, tfmatrix):
     """
     Apply the transformation matrix to the point cloud.
@@ -70,9 +72,10 @@ def transform_point_cloud(point_cloud, tfmatrix):
     # Convert to homogeneous coordinates for matrix multiplication
     pc_homogeneous = np.hstack((point_cloud, np.ones((point_cloud.shape[0], 1))))
     tf_pc_homogeneous = np.dot(tfmatrix, pc_homogeneous.T).T
-    tf_point_cloud = tf_pc_homogeneous[:, :3]   # Remove the homogeneous coordinate
-    
+    tf_point_cloud = tf_pc_homogeneous[:, :3]  # Remove the homogeneous coordinate
+
     return tf_point_cloud
+
 
 def solve_icp_with_known_correspondence(point_cloud1, point_cloud2):
     """
@@ -80,7 +83,7 @@ def solve_icp_with_known_correspondence(point_cloud1, point_cloud2):
     """
     # Solve for transformation matrix
     T1_2 = icp_core(point_cloud1, point_cloud2)
-    print('------------ transformation matrix T1_2 ------------')
+    print("------------ transformation matrix T1_2 ------------")
     print(T1_2)
 
     start_time = datetime.datetime.now()
@@ -90,32 +93,35 @@ def solve_icp_with_known_correspondence(point_cloud1, point_cloud2):
 
     end_time = datetime.datetime.now()
     time_cost = (end_time - start_time).total_seconds()
-    print('time cost = ' + str(time_cost) + 's')
-    
+    print("time cost = " + str(time_cost) + "s")
+
     # Visualization
     mean_distance = mean_dist(point_cloud2_transformed, point_cloud1)
-    print('mean_error= ' + str(mean_distance))
-    print('Red for reference cloud')
-    print('Green for original cloud')
-    print('Blue for transformed cloud')
-    print('Red and blue should overlap')
+    print("mean_error= " + str(mean_distance))
+    print("Red for reference cloud")
+    print("Green for original cloud")
+    print("Blue for transformed cloud")
+    print("Red and blue should overlap")
 
-    axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(size=10, origin=[0, 0, 0])
-    
+    axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=10, origin=[0, 0, 0]
+    )
+
     pcd1 = o3d.geometry.PointCloud()
     pcd1.points = o3d.utility.Vector3dVector(point_cloud1)
     pcd2 = o3d.geometry.PointCloud()
     pcd2.points = o3d.utility.Vector3dVector(point_cloud2)
     pcd2_transformed = o3d.geometry.PointCloud()
     pcd2_transformed.points = o3d.utility.Vector3dVector(point_cloud2_transformed)
-    
+
     # Color coding for visualization
     pcd1.paint_uniform_color([1, 0, 0])  # Red for reference cloud
     pcd2.paint_uniform_color([0, 1, 0])  # Green for original cloud
     pcd2_transformed.paint_uniform_color([0, 0, 1])  # Blue for transformed cloud
-    
+
     # Display point clouds
     o3d.visualization.draw_geometries([pcd1, pcd2, pcd2_transformed, axis_pcd])
+
 
 def estimate_correspondence(point_cloud1, point_cloud2):
     """
@@ -133,7 +139,10 @@ def estimate_correspondence(point_cloud1, point_cloud2):
 
     return point_cloud2_reordered
 
-def solve_icp_without_known_correspondence(point_cloud1, point_cloud2, n_iter, threshold):
+
+def solve_icp_without_known_correspondence(
+    point_cloud1, point_cloud2, n_iter, threshold
+):
     """
     Solve ICP without known correspondences by iteratively refining correspondences.
     """
@@ -141,7 +150,9 @@ def solve_icp_without_known_correspondence(point_cloud1, point_cloud2, n_iter, t
     T_1_2accumulated = np.eye(4)
 
     # viz
-    axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(size=10, origin=[0, 0, 0])
+    axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame(
+        size=10, origin=[0, 0, 0]
+    )
     pcd1 = o3d.geometry.PointCloud()
     pcd1.points = o3d.utility.Vector3dVector(point_cloud1)
     pcd1.paint_uniform_color([0, 0, 1])
@@ -149,45 +160,47 @@ def solve_icp_without_known_correspondence(point_cloud1, point_cloud2, n_iter, t
     vis.create_window()
     vis.add_geometry(axis_pcd)
     vis.add_geometry(pcd1)
-    
+
     total_time_cost = 0
 
     # ICP Iterations
     for i in range(n_iter):
         start_time = datetime.datetime.now()
-        
-        # TODO: Try to estimate correspondence of points between 2 point clouds, 
+
+        # TODO: Try to estimate correspondence of points between 2 point clouds,
         #       and reindex point_cloud2 based on your estimated correspondence
         # 1. Estimate correspondences using KD-Tree
-        point_cloud2_reordered = estimate_correspondence(point_cloud1, point_cloud2_temp)
-        
+        point_cloud2_reordered = estimate_correspondence(
+            point_cloud1, point_cloud2_temp
+        )
+
         # 2. Solve ICP for the current iteration
         T1_2_cur = icp_core(point_cloud1, point_cloud2_reordered)
-        
+
         end_time = datetime.datetime.now()
         time_difference = (end_time - start_time).total_seconds()
         total_time_cost += time_difference
-        
+
         # TODO: Update accumulated transformation
         # 3. Accumulate transformations
         T_1_2accumulated = np.dot(T1_2_cur, T_1_2accumulated)
-        
-        print('-----------------------------------------')
-        print('iteration = ' + str(i+1))
-        print('time cost = ' + str(time_difference) + 's')
-        print('total time cost = ' + str(total_time_cost) + 's')        
-        print('T1_2_cur = ')
+
+        print("-----------------------------------------")
+        print("iteration = " + str(i + 1))
+        print("time cost = " + str(time_difference) + "s")
+        print("total time cost = " + str(total_time_cost) + "s")
+        print("T1_2_cur = ")
         print(T1_2_cur)
-        print('accumulated T = ')
+        print("accumulated T = ")
         print(T_1_2accumulated)
-        
+
         # TODO: Update point cloud2 using transform from current iteration
         # 4. Update the point cloud with the new transformation
         point_cloud2_temp = transform_point_cloud(point_cloud2, T_1_2accumulated)
-        
+
         # 5. Calculate mean distance between the point clouds
         mean_distance = mean_dist(point_cloud1, point_cloud2_temp)
-        print('mean_error= ' + str(mean_distance))
+        print("mean_error= " + str(mean_distance))
 
         # 6. Update visualization
         pcd2_transed = o3d.geometry.PointCloud()
@@ -200,21 +213,22 @@ def solve_icp_without_known_correspondence(point_cloud1, point_cloud2, n_iter, t
 
         # 7. Check convergence
         if mean_distance < 0.00001 or mean_distance < threshold:
-            print('------- fully converged! -------')
+            print("------- fully converged! -------")
             break
-        
-        if i == n_iter - 1:
-            print('------- reach iteration limit -------')
 
-    print('time cost: ' + str(total_time_cost) + ' s')
-    
+        if i == n_iter - 1:
+            print("------- reach iteration limit -------")
+
+    print("time cost: " + str(total_time_cost) + " s")
+
     vis.destroy_window()
-    
+
     # Final visualization
     pcd2_final = o3d.geometry.PointCloud()
     pcd2_final.points = o3d.utility.Vector3dVector(point_cloud2_temp)
     pcd2_final.paint_uniform_color([1, 0, 0])
     o3d.visualization.draw_geometries([axis_pcd, pcd1, pcd2_final])
+
 
 def mean_dist(point_cloud1, point_cloud2):
     """
@@ -225,13 +239,18 @@ def mean_dist(point_cloud1, point_cloud2):
         dif = point_cloud1[i] - point_cloud2[i]
         dis = np.linalg.norm(dif)
         dis_array.append(dis)
-        
+
     return np.mean(np.array(dis_array))
 
+
 def main():
-    print('start hw program')
-    pcd1 = o3d.io.read_point_cloud('student_data/student_data_65/bunny1.ply') # change to your file path
-    pcd2 = o3d.io.read_point_cloud('student_data/student_data_65/bunny2.ply') # change to your file path
+    print("start hw program")
+    pcd1 = o3d.io.read_point_cloud(
+        "student_data/student_data_65/bunny1.ply"
+    )  # change to your file path
+    pcd2 = o3d.io.read_point_cloud(
+        "student_data/student_data_65/bunny2.ply"
+    )  # change to your file path
     points1 = np.array(pcd1.points)
     points2 = np.array(pcd2.points)
 
@@ -242,5 +261,6 @@ def main():
     # solve_icp_without_known_correspondence(points1, points2, n_iter=100, threshold=0.1)
     # solve_icp_without_known_correspondence(points1, points2, n_iter=200, threshold=0.01)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
