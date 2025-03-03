977e2dc56879ffda484b41dac564942f6860d487
“YukiKuma111” <1343879876@qq.com>
2025-03-04 04:10:17 +0800

    Tune ^Crtographer

--- task2/catkin_ws/install_isolated/share/cartographer_ros/configuration_files/jackal_robot.lua
+++ task2/catkin_ws/install_isolated/share/cartographer_ros/configuration_files/jackal_robot.lua
@@ -1,16 +1,16 @@
--- Copyright 2016 The Cartographer Authors
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---      http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- -- Copyright 2016 The Cartographer Authors
+-- --
+-- -- Licensed under the Apache License, Version 2.0 (the "License");
+-- -- you may not use this file except in compliance with the License.
+-- -- You may obtain a copy of the License at
+-- --
+-- --      http://www.apache.org/licenses/LICENSE-2.0
+-- --
+-- -- Unless required by applicable law or agreed to in writing, software
+-- -- distributed under the License is distributed on an "AS IS" BASIS,
+-- -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- -- See the License for the specific language governing permissions and
+-- -- limitations under the License.
 
 include "map_builder.lua"
 include "trajectory_builder.lua"
@@ -19,11 +19,10 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link", -- imu frame
-  published_frame = "base_link",
+  tracking_frame = "base_link",
+  published_frame = "odom",
   odom_frame = "odom",
-  -- provide_odom_frame = false,
-  provide_odom_frame = true,
+  provide_odom_frame = false,
   publish_frame_projected_to_2d = false,
   use_pose_extrapolator = true,
   use_odometry = true,
@@ -44,8 +43,23 @@ options = {
   landmarks_sampling_ratio = 1.,
 }
 
+MAP_BUILDER.num_background_threads = 4 
 MAP_BUILDER.use_trajectory_builder_2d = true
-TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+
+-- TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.2
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.0)
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 5000
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.3
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(25.)
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 10.
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 3.
+
+POSE_GRAPH.optimization_problem.huber_scale = 1e2
+POSE_GRAPH.optimize_every_n_nodes = 1000
+POSE_GRAPH.constraint_builder.min_score = 0.65
+POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
+POSE_GRAPH.constraint_builder.max_constraint_distance = 30.
 
 return options
